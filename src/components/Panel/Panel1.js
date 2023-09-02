7a06cbf3b285738f7c78e359a05df56f8423720e
Karan Kharadi <karan.k@tyrannix.com>
2023-09-02 19:01:27 +0530

    panel page almost done

--- src/components/Panel/Panel1.js
+++ src/components/Panel/Panel1.js
@@ -1,9 +1,11 @@
 import React,{useState} from 'react'
 import Box from '@mui/material/Box'
 import Typography  from '@mui/material/Typography'
+import Toolbar from '@mui/material/Toolbar'
 import Button from '@mui/material/Button'
 import Paper from '@mui/material/Paper'
 import Stack from '@mui/material/Stack'
+import Divider from '@mui/material/Divider'
 import { styles } from './PanelPageStyles'
 import sparkline from '../image/Sparkline.png'
 import bluestroke from '../image/bluestroke.png'
@@ -15,9 +17,10 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import {createTheme,ThemeProvider,useTheme} from '@mui/material/styles'
-import InputAdornment from "@mui/material/InputAdornment";
-import AddIcon from "@mui/icons-material/Add";
+import { TableRow, TableBody, TableCell } from '@mui/material'
+import useTable from '../control/PanelTable'
 import date from '../image/date.png'
+import filterIcon from '../image/filterButtonIcon.png'
 import './Panel.css'
 const style =styles()
 
@@ -34,8 +37,14 @@ createTheme({
                 '--TextField-brandBorderHoverColor': '#919EAB33',
                 '--TextField-brandBorderFocusedColor': '#919EAB33',
                 '& label.Mui-focused': {
-                  color: '#637381' , //'var(--TextField-brandBorderFocusedColor)'
+                  color: '#919EAB' , //'var(--TextField-brandBorderFocusedColor)'
                 },
+                '& label.MuiFormLabel-root':{
+                  color:'#919EAB',
+                  fontFamily:'Public Sans',
+                  fontSize:'14px',
+                  fontWeight:400
+                }
               },
             },
           },
@@ -51,16 +60,97 @@ createTheme({
             '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
               borderColor: 'var(--TextField-brandBorderFocusedColor)',
             },
+            '& .MuiOutlinedInput-input':{
+              fontFamily:'Public Sans',
+              fontWeight:400,
+              fontSize:'14px',
+              color:'#212B36'
+            }
           },
             },
           },
       },
 });
+
+//-------------Time------------------------------//
+const formatTime = () => {
+  const date = new Date();
+  const options = { hour: 'numeric', minute: 'numeric', hour12: true };
+  return new Intl.DateTimeFormat('en-US', options).format(date);
+};
+//---------------Time----------------------------//
+//---------------Date---------------------------//
+const getFormattedDate = () => {
+  const months = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
+
+  const currentDate = new Date();
+  const day = currentDate.getDate();
+  const month = months[currentDate.getMonth()];
+  const year = currentDate.getFullYear();
+
+  return `${day} ${month} ${year}`;
+};
+//--------------Date----------------------//
+
+const mergedTime=()=>{
+  const date= getFormattedDate()
+  const time= formatTime()
+  const mergedDateAndTime=`${date}-${time}`
+  return mergedDateAndTime;
+}
+
+function createData(transactionAmount, date, utrNumber,user) {
+  return {transactionAmount, date, utrNumber,user };
+}
+
+const headCells=[
+  {id:'transactionAmount', label:'Transcation Amount'},
+  {id:'date', label:'Date'},
+  {id:'utrNumber', label:'UTR Number'},
+  {id:'user', label:'User'},
+]
+const recordsData=[
+  createData('40,000',mergedTime(),5412,'suraj2241'),
+  createData('30,000',mergedTime(),9412,'rakesh678'),
+  createData('25,000',mergedTime(),7452,'kalpu765'),
+  createData('20,000',mergedTime(),9574,'kevin246'),
+  createData('10,000',mergedTime(),6214,'harsh567'),
+  createData('40,000',mergedTime(),5412,'suraj2241'),
+  createData('30,000',mergedTime(),9412,'rakesh678'),
+  createData('25,000',mergedTime(),7452,'kalpu765'),
+  createData('20,000',mergedTime(),9574,'kevin246'),
+  createData('10,000',mergedTime(),6214,'harsh567'),
+]
+
 const Panel1 = () => {
+  const[records,setRecords]=useState(recordsData)
+  const [filterFn, setFilterFn] = useState({ fn: items => { return items; } })
   const [option, setOption]= useState('Withdraw')
   const handleOption=(optionName)=>{
     setOption(optionName)
   }
+
+  const {
+    TblContainer,
+    TblHead,
+    TblPagination,
+    recordsAfterPagingAndSorting,
+} = useTable(records, headCells, filterFn);
+
+const handleSearch = e => {
+  let target = e.target;
+  setFilterFn({
+      fn: items => {
+          if (target.value === "")
+              return items;
+          else
+              return items.filter(x => x.transactionAmount.includes(target.value))
+      }
+  })
+}
   return (
    <div>
     <Box>
@@ -70,61 +160,104 @@ const Panel1 = () => {
       </Box>
       <Box sx={style.cardContainer}>
         <Box sx={style.card}>
-          <Box sx={{border:'2px solid green',display:'flex',flexDirection:'column',justifyContent:'space-between',width:'75%',height:'60%'}}>
-            <Typography sx={{fontFamily:'Public Sans',fontSize:'18px',fontWeight:700,color:'#212B36'}}>Total Points</Typography>
-            <Typography sx={{fontFamily:'Public Sans',fontSize:'28px',fontWeight:700,color:'#212B36'}}>4,20,00,000</Typography>
+          <Box sx={{display:'flex',flexDirection:'column',justifyContent:'space-between',width:'75%',height:'50%'}}>
+            <Typography sx={{fontFamily:'Public Sans',fontSize:'15px',fontWeight:700,color:'#212B36'}}>Total Points</Typography>
+            <Typography sx={{fontFamily:'Public Sans',fontSize:'25px',fontWeight:700,color:'#212B36'}}>4,20,00,000</Typography>
           </Box>
-          <Box sx={{display:'flex',alignItems:'center',border:'2px solid black',width:'20%',height:'100%'}}>
+          <Box sx={{display:'flex',alignItems:'center',width:'20%',height:'100%'}}>
             <img src={sparkline} style={{width:'100%'}}/>
           </Box>
 
         </Box>
          <Box sx={style.card}>
-         <Box sx={{border:'2px solid green',display:'flex',flexDirection:'column',justifyContent:'space-between',width:'75%',height:'70%'}}>
-            <Typography sx={{fontFamily:'Public Sans',fontSize:'14px',fontWeight:700,color:'#212B36'}}>Analytics 2</Typography>
-            <Typography sx={{fontFamily:'Public Sans',fontSize:'28px',fontWeight:700,color:'#212B36'}}>48,951</Typography>
-            <Box sx={{border:'2px solid black',display:'flex',justifyContent:'space-between',alignItems:'center'}}>
+         <Box sx={{display:'flex',flexDirection:'column',justifyContent:'space-around',width:'75%',height:'65%'}}>
+            <Typography sx={{fontFamily:'Public Sans',fontSize:'12px',fontWeight:600,color:'#212B36'}}>Analytics 2</Typography>
+            <Typography sx={{fontFamily:'Public Sans',fontSize:'25px',fontWeight:700,color:'#212B36'}}>48,951</Typography>
+            <Box sx={{display:'flex',justifyContent:'space-between',alignItems:'center'}}>
               <img src={downcircle}/>
-              <Typography sx={{fontFamily:'Public Sans',fontSize:'13px',fontWeight:600,color:'#212B36'}}>-2.4%</Typography>
-              <Typography sx={{fontFamily:'Public Sans',fontSize:'13px',fontWeight:400,color:'#637381'}}>than last week</Typography>
+              <Typography sx={{fontFamily:'Public Sans',fontSize:'12px',fontWeight:600,color:'#212B36'}}>-2.4%</Typography>
+              <Typography sx={{fontFamily:'Public Sans',fontSize:'12px',fontWeight:400,color:'#637381'}}>than last week</Typography>
             </Box>
           </Box>
-          <Box sx={{display:'flex',alignItems:'center',border:'2px solid black',width:'20%',height:'100%'}}>
+          <Box sx={{display:'flex',alignItems:'center',width:'20%',height:'100%'}}>
             <img src={bluestroke} style={{width:'100%'}}/>
           </Box>
         </Box>
          <Box sx={style.card}>
-         <Box sx={{border:'2px solid green',display:'flex',flexDirection:'column',justifyContent:'space-between',width:'75%',height:'70%'}}>
-            <Typography sx={{fontFamily:'Public Sans',fontSize:'14px',fontWeight:700,color:'#212B36'}}>Analytics 2</Typography>
-            <Typography sx={{fontFamily:'Public Sans',fontSize:'28px',fontWeight:700,color:'#212B36'}}>28,971</Typography>
-            <Box sx={{border:'2px solid black',display:'flex',justifyContent:'space-between',alignItems:'center'}}>
+         <Box sx={{display:'flex',flexDirection:'column',justifyContent:'space-around',width:'75%',height:'65%'}}>
+            <Typography sx={{fontFamily:'Public Sans',fontSize:'12px',fontWeight:600,color:'#212B36'}}>Analytics 3</Typography>
+            <Typography sx={{fontFamily:'Public Sans',fontSize:'25px',fontWeight:700,color:'#212B36'}}>28,971</Typography>
+            <Box sx={{display:'flex',justifyContent:'space-between',alignItems:'center'}}>
               <img src={upcircle}/>
-              <Typography sx={{fontFamily:'Public Sans',fontSize:'13px',fontWeight:600,color:'#212B36'}}>+9.4%</Typography>
-              <Typography sx={{fontFamily:'Public Sans',fontSize:'13px',fontWeight:400,color:'#637381'}}>than last week</Typography>
+              <Typography sx={{fontFamily:'Public Sans',fontSize:'12px',fontWeight:600,color:'#212B36'}}>+9.4%</Typography>
+              <Typography sx={{fontFamily:'Public Sans',fontSize:'12px',fontWeight:400,color:'#637381'}}>than last week</Typography>
             </Box>
           </Box>
-          <Box sx={{display:'flex',alignItems:'center',border:'2px solid black',width:'20%',height:'100%'}}>
+          <Box sx={{display:'flex',alignItems:'center',width:'20%',height:'100%'}}>
             <img src={yellowstroke} style={{width:'100%'}}/>
           </Box>
         </Box>
       </Box>
-      <Paper sx={{borderRadius:'16px',paddingTop:'10px',marginTop:'10px'}}>
-      <Stack direction="row"alignItems='center' spacing={2} sx={{height:'30px',marginLeft:'10px'}}>
+      <Paper sx={{borderRadius:'16px',marginTop:'10px'}}>
+      <Stack direction="row"alignItems='center' spacing={2} sx={{height:'30px',paddingTop:'8px',marginLeft:'15px'}}>
         <Typography sx={option==='Withdraw'?style.tableHeadActiveText:style.tableHeadText} onClick={()=>handleOption('Withdraw')}>Withdraw</Typography>
         <Typography sx={option==='Deposit'?style.tableHeadActiveText:style.tableHeadText} onClick={()=>handleOption('Deposit')} >Deposit</Typography>
       </Stack>
+      <Divider sx={{marginBottom:'10px'}}/>
+      <Toolbar sx={{
+                    '& .MuiStack-root':{
+                      marginRight:'10px'
+                     }
+          }}>
       <ThemeProvider theme={customTheme1()} >  
         <LocalizationProvider dateAdapter={AdapterDayjs}>
         <DemoContainer components={["DatePicker"]}>
-          <DatePicker label="End Date"   
-           slots={{
-            openPickerIcon:AddIcon
-           }}
+          <DatePicker label="Start Date"   
            sx={{width:'260px'}}
           />
         </DemoContainer>
       </LocalizationProvider>
       </ThemeProvider>
+      <ThemeProvider theme={customTheme1()} >  
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DemoContainer components={["DatePicker"]}>
+          <DatePicker label="End Date"   
+           sx={{width:'260px'}}
+          />
+        </DemoContainer>
+      </LocalizationProvider>
+      </ThemeProvider>
+      <Button sx={style.filterButton} variant='contained' endIcon={<img src={filterIcon}/>}>
+        <Typography sx={style.filterButtonText}>Filters</Typography>
+      </Button>
+      </Toolbar>
+      <TblContainer>
+      <TblHead />
+      <TableBody>
+          {
+              recordsAfterPagingAndSorting().map((item,index) =>
+              (<TableRow key={index}>
+                      <TableCell sx={{minWidth:50,width:100,textAlign:'center'}}><Typography sx={style.tableDataText} >{item.transactionAmount}</Typography></TableCell>
+                      <TableCell sx={{minWidth:50,width:150, textAlign:'center'}}>
+                      <Typography sx={style.DateText} >{item.date.split('-')[0]}</Typography>
+                      <Typography sx={style.TimeText} >{item.date.split('-')[1]}</Typography>
+                      </TableCell>
+                      <TableCell sx={{minWidth:50,width:130, textAlign:'center'}}><Typography sx={style.tableDataText} >{item.utrNumber}</Typography></TableCell>
+                      <TableCell sx={{minWidth:100,width:1000, textAlign:'center'}}>
+                          <Box sx={style.userEditButtonBox}>
+                          <Typography sx={style.tableDataText} >{item.user}</Typography>
+                          <Button variant='contained' sx={style.userEditButton}>
+                              <Typography sx={style.userEditButtonText}>Edit</Typography>
+                          </Button>
+                          </Box>
+                      </TableCell>
+                  </TableRow>)
+              )
+          }
+      </TableBody>
+      </TblContainer>
+      <TblPagination
+      />
       </Paper>
     </Box>
    </div>
--- src/components/Panel/PanelPageStyles.js
+++ src/components/Panel/PanelPageStyles.js
@@ -29,19 +29,22 @@ export const styles=()=>({
         color:'#212B36'
     },
     cardContainer:{
-        border:'2px solid black',
+        // border:'2px solid black',
         width:'100%',
         height:'150px',
         display:'flex',
-        gap:'10px', 
+        gap:'20px', 
     },
     card:{
         width:'30%',
         height:'100%',
-        border:'2px solid red',
+        // border:'2px solid red',
         display:'flex',
         justifyContent:'space-between',
-        alignItems:'center'
+        alignItems:'center',
+        borderRadius:'16px',
+        boxShadow:'0px 4px 8px 0px #919EAB29',
+        padding:'0px 16px 0px 16px'
     },
     tableHeadText:{
         //styleName: Desktop/Subtitle2;
@@ -67,4 +70,71 @@ export const styles=()=>({
     borderBottom:'2px solid black',
     cursor:'pointer',
     },
+    filterButton:{
+    textTransform:'none',
+    width:'94px',
+    height:'40px',
+    padding: '6px 12px 6px 12px',
+    borderRadius: '8px',
+    border: '1px',
+    gap: '8px',
+    marginLeft:'10px',
+    background:'white',
+    '&:hover':{
+    background:'grey'
+    }
+    },
+    filterButtonText:{
+    color:'#919EAB',  //styleName: Desktop/Body2;
+    fontFamily: 'Public Sans',
+    fontSize: '14px',
+    fontWeight: '400',
+    letterSpacing: '0px',
+    textAlign: 'center',
+    },
+    tableDataText:{
+        fontFamily: 'Public Sans',
+        fontSize: '14px',
+        fontWeight: 400,
+        lineHeight: '22px',
+        letterSpacing: '0px',
+        textAlign: 'center',
+    },
+    DateText:{
+        fontFamily:'Public Sans',
+        fontSize:'14px',
+        fontWeight:400,
+        color:'#212B36',
+    },
+    TimeText:{
+        fontFamily:'Public Sans',
+        fontSize:'14px',
+        fontWeight:400,
+        marginLeft:'-50px',
+        color:'#919EAB',
+    },
+    userEditButtonBox:{
+        display:'flex',
+        justifyContent:'space-between',
+        border:'2px solid green',
+        alignItems:'center'
+       },
+       userEditButton:{
+        width:'43px',
+        height:'30px',
+        borderRadius: '8px',
+        background:'#919EAB14',
+        minWidth:0,
+        '&:hover':{
+        background:'grey'
+        }
+       },
+       userEditButtonText:{
+        fontFamily: 'Public Sans',
+        fontSize: '14px',
+        fontWeight: 600,
+        lineHeight:'22px',
+        textTransform:'none',
+        color:'#212B36'
+       }
 })
